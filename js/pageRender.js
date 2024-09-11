16533e1dbd77170684723a3a5d0867d663da3237
TheOmidious <omidpournejati@gmail.com>
2024-09-11 18:15:16 +0200

    fixed folder structure, added js file

---
+++ js/pageRender.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('container');
+
+// Create an img element
+const img = document.createElement('img');
+
+// Set the src attribute for the image
+img.src = '/assets/images/Test.jpeg';
+
+// Append the image to the container
+container.appendChild(img);
+
+
